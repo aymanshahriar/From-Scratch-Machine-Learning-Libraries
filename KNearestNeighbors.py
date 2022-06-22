8ec14336715a9b81a64cf82d8582474934e91278
aymanshahriar <ayman.shahriar@ucalgary.ca>
2022-06-22 08:19:17 -0600

    Finished KNearestClassifier

--- KNearestNeighbors.py
+++ KNearestNeighbors.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from collections import Counter
-import csv
+
 
 class MyKNearestClassifier:
 
@@ -13,10 +13,12 @@ class MyKNearestClassifier:
     # Get distance between two n-dimensional points
     # (assume they are 1d arrays only containing the features, not target)
     def get_distance(self, datapointA, datapointB):
-        # Make sure data type to int64 to prevent overflow. By default its int32
-        datapointA = datapointA.astype('int64')
-        datapointB = datapointB.astype('int64')
+        # If using numpy arrays, make sure data type to int64 to prevent overflow. By default its int32
+        # Instead, I just converted to lists, keeps things simple. Besides, int64 cannot handle decimals.
+        datapointA = list(datapointA)
+        datapointB = list(datapointB)
         sum_squared_diff = 0
+
         for featureA, featureB in zip(datapointA, datapointB):
             squared_diff = (featureA - featureB) ** 2
             sum_squared_diff += squared_diff
@@ -43,9 +45,9 @@ class MyKNearestClassifier:
         minmax_normalized_X = np.array(list(zip(*normalized_features)))
         return minmax_normalized_X
 
-    def fit(self, X_train, Y_train):
+    def fit(self, X_train, y_train):
         self.X_train = X_train
-        self.Y_train = Y_train
+        self.y_train = y_train
 
     # For an unknown datapoint, find its nearest k neighbors.
     # Classify the new point based on those neighbors
@@ -53,28 +55,101 @@ class MyKNearestClassifier:
         # Find the distance between this datapoint and every other datapoint.
         # Distances is a 2d array, storing [distance, target] of each datapoint.
         distances = []
-        for known_datapoint in self.X_train:
+        for known_datapoint, target in zip(self.X_train, self.y_train):
             distance = self.get_distance(unknown, known_datapoint)
-            distances.append(distance)
+            distances.append([distance, target])
+        # Sort the distances, return the distance. By default, I think a 2d array will be sorted by the first element
+        # in the subarray.
+        distances.sort()
+        neighbors = distances[0:self.k]
+        # Now count the most common class/category among the neighbors, and that will be our predicted category
+        # for the unknown datapoint.
+        neighbors_features = [x[1] for x in neighbors]
+        counter = Counter(neighbors_features)
+        most_common_feature = counter.most_common(1)[0][0]
+        return most_common_feature
+
+    def predict(self, X_test):
+        y_pred = []
+        for unknown_datapoint in X_test:
+            predicted_target = self.predict_single_datapoint(unknown_datapoint)
+            y_pred.append(predicted_target)
+        return np.array(y_pred)
+
+
+class MyKNearestRegressor:
+
+    def __init__(self, k=5):
+        self.k = k
+        self.X_train = None
+        self.y_train = None
+
+    # Get distance between two n-dimensional points
+    # (assume they are 1d arrays only containing the features, not target)
+    def get_distance(self, datapointA, datapointB):
+        # If using numpy arrays, make sure data type to int64 to prevent overflow. By default its int32
+        # Instead, I just converted to lists, keeps things simple. Besides, int64 cannot handle decimals.
+        datapointA = list(datapointA)
+        datapointB = list(datapointB)
+        sum_squared_diff = 0
+
+        for featureA, featureB in zip(datapointA, datapointB):
+            squared_diff = (featureA - featureB) ** 2
+            sum_squared_diff += squared_diff
+        distance = sum_squared_diff ** 0.5
+        return distance
+
+    # Min-max normalize:
+    # Set the min to be 0 and max to be 1. The other numbers will transform to values between 0 and 1, depending on their distance from the min and max
+    # Input will be a 2d matrix of features, where each row corresponds to the features of a single datapoint
+    def minmax_normalize(self, X):
+        # In order to loop through each column, just loop through the transposed matrix
+        # A transposed matrix is just a new matrix where the rows of original matrix are columns and vice versa.
+        transposed_X = zip(*X)
+        normalized_features = []
+        for feature in transposed_X:
+            normalized_feature = []
+            minimum = min(feature)
+            maximum = max(feature)
+            for value in feature:
+                normalized_value = (value-minimum)/(maximum-minimum)
+                normalized_feature.append(normalized_value)
+            normalized_features.append(normalized_feature)
+        # Transpose again to make sure each row corresponds to the features of a single datapoint
+        minmax_normalized_X = np.array(list(zip(*normalized_features)))
+        return minmax_normalized_X
+
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    # For an unknown datapoint, find its nearest k neighbors.
+    # Classify the new point based on those neighbors
+    def predict_single_datapoint(self, unknown):
+        # Find the distance between this datapoint and every other datapoint.
+        # Distances is a 2d array, storing [distance, target] of each datapoint.
+        distances = []
+        for known_datapoint, target in zip(self.X_train, self.y_train):
+            distance = self.get_distance(unknown, known_datapoint)
+            distances.append([distance, target])
         # Sort the distances, return the distance. By default, I think a 2d array will be sorted by the first element
         # in the subarray.
         distances.sort()
         neighbors = distances[0:self.k]
 
-        # Now count the most common class/category among the neighbors, and that will be out predicted category
-        # for the unknown datapoint.
-        neighbors_features = neighbors[:, 1]
-        counter = Counter(neighbors_features)
-        most_common_feature = counter.most_common(1)[0][0]
-        return most_common_feature
+        # Now get the average of the neighbor's targets, that will be our predicted target for the unlabelled datapoint
+        sum_neighbor_targets = 0
+        for neighbor in neighbors:
+            sum_neighbor_targets += neighbor[1]
+        mean_neighbor_targets = sum_neighbor_targets / k
+        return mean_neighbor_targets
 
-
-
-
-
-
-
-
+    def predict(self, X_test):
+        y_pred = []
+        for unknown_datapoint in X_test:
+            predicted_target = self.predict_single_datapoint(unknown_datapoint)
+            y_pred.append(predicted_target)
+        return np.array(y_pred)
 
 
 
---
+++ Test KNearestClassifier/TestPredictSingleDatapoint.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from KNearestNeighbors import MyKNearestClassifier
+
+############################################################################################################
+def distance(movie1, movie2):
+  squared_difference = 0
+  for i in range(len(movie1)):
+    squared_difference += (movie1[i] - movie2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def classify(unknown, dataset, labels, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #print(distances[0:k+1])
+  #print([labels[title] for title in [a[1] for a in distances[0:k+1]]])
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  num_good = 0
+  num_bad = 0
+  for neighbor in neighbors:
+    title = neighbor[1]
+    if labels[title] == 0:
+      num_bad += 1
+    elif labels[title] == 1:
+      num_good += 1
+  if num_good > num_bad:
+    return 1
+  else:
+    return 0
+
+########################################################################################################
+dataframe = pd.read_csv("data.csv")
+X = dataframe[['feature1', 'feature2', 'feature3']].values
+y = dataframe['target'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+
+X_dict = {}
+for title, features in zip(dataframe['title'], X_train):
+    X_dict[title] = list(features)
+
+y_dict = {}
+for title, target in zip(dataframe['title'], y_train):
+    y_dict[title] = target
+
+y_pred_sololearn = []
+for unknown_datapoint in X_test:
+    predicted_target = classify(list(unknown_datapoint), X_dict, y_dict, 3)
+    y_pred_sololearn.append(predicted_target)
+
+##################################################################
+
+model = MyKNearestClassifier(k=3)
+model.fit(X_train, y_train)
+
+y_pred_me = []
+for unknown_datapoint in X_test:
+    predicted_target = model.predict_single_datapoint(unknown_datapoint)
+    y_pred_me.append(predicted_target)
+
+for a, b in zip(y_pred_me, y_pred_sololearn):
+    print(a, b)
+
+#print(model.predict_single_datapoint(X_test[-5]), classify(list(X_test[-5]), X_dict, y_dict, 3))
